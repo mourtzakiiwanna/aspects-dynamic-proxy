e65a191736e8179b8266a57a4dc364b574d1d90a
Ioanna Mourtzaki <109597293+mourtzakiiwanna@users.noreply.github.com>
2023-02-23 21:55:33 +0200

    Update SimpleGreeting.java

--- SimpleGreeting.java
+++ SimpleGreeting.java
@@ -1,7 +1,6 @@
 public class SimpleGreeting implements Greeting {
     @Override
     public String greet(String name) {
-        //System.out.println("Hello" + name +"!");
         return ("Hello " + name +"!");
     }
 }
